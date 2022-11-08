6995524e97e7dec6fefb27be17cb480c4cd8fe2f
M. Valkenburg <m.valkenburg@live.nl>
2022-11-08 22:26:25 +0100

    deployment lines

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "https://m-valkenburg.github.io/Rainbow-Six-Extraction-operators" : "/",
+  publicPath: "/Rainbow-Six-Extraction-operators/",
 })
